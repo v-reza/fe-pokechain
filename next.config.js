b4ee9b8dba6978096491a0f1e7e54304e9bf4031
v-reza <mrezalf0@gmail.com>
2022-10-17 17:50:54 +0700

    eslint disable production

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    CI: false,
+    GENERATE_SOURCEMAP: false
+  }
 }
 
 module.exports = nextConfig
